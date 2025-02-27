9807124890bbbf79efa52b561eee07be3c4a623d
manumafe98 <manumaxerapro@gmail.com>
2025-02-26 23:11:02 -0300

    Add movie and test addition

--- contracts/contracts/HoytsX.sol
+++ contracts/contracts/HoytsX.sol
@@ -4,9 +4,68 @@ pragma solidity ^0.8.28;
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract HoytsX is ERC721 {
+    address public owner;
+    uint16 public totalMovies;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
-
+    struct Movie {
+        uint16 id;
+        string name;
+        string description;
+        string ipfsHash;
+        string genre;
+        string director;
+        string[] actors;
+        string date;
+        string time;
+        uint256 cost;
+        uint16 duration;
+        uint8 tickets;
+        uint8 maxTickets;
     }
 
+    mapping(uint16 => Movie) movies;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function listMovie(
+        string memory _name,
+        string memory _description,
+        string memory _ipfsHash,
+        string memory _genre,
+        string memory _director,
+        string[] memory _actors,
+        string memory _date,
+        string memory _time,
+        uint256 _cost,
+        uint16 _duration,
+        uint8 _maxTickets
+    ) public onlyOwner {
+        totalMovies += 1;
+        movies[totalMovies] = Movie(
+            totalMovies,
+            _name,
+            _description,
+            _ipfsHash,
+            _genre,
+            _director,
+            _actors,
+            _date,
+            _time,
+            _cost,
+            _duration,
+            _maxTickets,
+            _maxTickets
+        );
+    }
+
+    function getMovie(uint16 _id) public view returns (Movie memory) {
+        return movies[_id];
+    }
 }
--- contracts/test/HoytsX.ts
+++ contracts/test/HoytsX.ts
@@ -1,16 +1,49 @@
 import { expect } from "chai";
 import hre from "hardhat";
 import { HoytsX } from "../typechain-types";
+import { Signer } from "ethers";
 
 const NAME = "HoytsX";
 const SYMBOL = "HX";
 
+const MOVIE_NAME = "Interstellar"
+const MOVIE_DESCRIPTION = "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans."
+const MOVIE_IPFS_HASH = ""
+const MOVIE_GENRE = "Sci-Fi"
+const MOVIE_DIRECTOR = "Christopher Nolan"
+const MOVIE_ACTORS = ["Matthew McConaughey", "Ann Hathaway", "Jessica Chastain"]
+const MOVIE_DATE = "Feb 27"
+const MOVIE_TIME = "10:00PM"
+const MOVIE_DURATION = 169
+const MOVIE_COST = hre.ethers.parseUnits("1", "ether")
+const MOVIE_MAX_TICKETS = 100
+
 describe("HoytsX", () => {
     let hoytsX: HoytsX;
+    let deployer: Signer, buyer: Signer;
 
     beforeEach(async () => {
+
+        [deployer, buyer] = await hre.ethers.getSigners();
+
         const HoytsXFactory  = hre.ethers.getContractFactory("HoytsX");
         hoytsX = await (await HoytsXFactory).deploy(NAME, SYMBOL);
+
+        const transaction = await hoytsX.connect(deployer).listMovie(
+            MOVIE_NAME,
+            MOVIE_DESCRIPTION,
+            MOVIE_IPFS_HASH,
+            MOVIE_GENRE,
+            MOVIE_DIRECTOR,
+            MOVIE_ACTORS,
+            MOVIE_DATE,
+            MOVIE_TIME,
+            MOVIE_COST,
+            MOVIE_DURATION,
+            MOVIE_MAX_TICKETS
+        )
+
+        await transaction.wait();
     })
 
     describe("Deployment", () => {
@@ -20,6 +53,18 @@ describe("HoytsX", () => {
     
         it("Sets the symbol", async () => {
             expect(await hoytsX.symbol()).to.equal(SYMBOL)
+        })
+
+        it("Sets the owner", async () => {
+            expect(await hoytsX.owner()).to.equal(await deployer.getAddress())
+        })
+    })
+
+    describe("Movie", () => {
+        it("Returns movie attributes", async () => {
+            const movie = await hoytsX.getMovie(1);
+            expect(movie.id).to.be.equal(1);
+            expect(movie.name).to.be.equal(MOVIE_NAME);
         })
     })
 })
