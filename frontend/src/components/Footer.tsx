d6a23ac43a4b1189999e3e764b837d01a2ac637f
manumafe98 <manumaxerapro@gmail.com>
2025-03-03 01:24:14 -0300

    Finish footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,16 +1,20 @@
+import { Facebook } from "../icons/Facebook";
+import { Instagram } from "../icons/Instagram";
+import { X } from "../icons/X";
+
 export const Footer = () => {
   return (
     <div className="flex justify-between items-center bg-[#121313] h-20 text-gray-500 mt-auto p-5">
       <h1 className="text-3xl font-bold">HoytsX</h1>
       <ul className="flex gap-2">
         <li>Copyright © {new Date().getFullYear()} National Amusements Inc. |</li>
-        <li>Privacy policies |</li>
-        <li>Book of complaints</li>
+        <li className="hover:underline hover:text-[#9b2b66]">Privacy policies |</li>
+        <li className="hover:underline hover:text-[#9b2b66]">Book of complaints</li>
       </ul>
-      <ul className="flex gap-2">
-        <li>Instagram</li>
-        <li>Facebook</li>
-        <li>Twitter</li>
+      <ul className="flex items-center gap-3">
+        <li><Instagram className="fill-current text-gray-500 h-6 w-6 hover:text-[#9b2b66]"/></li>
+        <li><Facebook className="fill-current text-gray-500 h-6 w-6 hover:text-[#9b2b66]"/></li>
+        <li><X className="fill-current text-gray-500 h-5 w-5 hover:text-[#9b2b66]"/></li>
       </ul>
     </div>
   )
