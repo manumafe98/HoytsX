fa9956f0b7db19b518a70a700faccdb4f6f875b7
manumafe98 <manumaxerapro@gmail.com>
2025-03-04 22:00:02 -0300

    Add bg color and border on hover

--- frontend/src/components/Movies.tsx
+++ frontend/src/components/Movies.tsx
@@ -15,10 +15,10 @@ export const Movies = () => {
   }, [])
 
   return (
-    <div className="grid grid-rows-2 grid-cols-5 py-5 px-10 gap-y-5 mx-auto place-items-center">
+    <div className="grid grid-rows-2 grid-cols-5 py-5 px-10 gap-y-5 mx-auto place-items-center bg-gradient-to-t from-[#163161] to-[#4469ac]">
       {movies.map((movie) => (
-        <div className="w-80 overflow-hidden rounded-xl hover:scale-105 transform duration-300">
-          <img className="object-cover" src={`https://dweb.link/ipfs/${movie.ipfsHash}`} alt={`${movie.name} film cover image`} />
+        <div className="w-80 h-12/12 overflow-hidden rounded-xl hover:scale-105 border-solid hover:border-4 border-[#9b2b66] ease-in duration-300">
+          <img className="w-full h-full object-cover" src={`https://dweb.link/ipfs/${movie.ipfsHash}`} alt={`${movie.name} film cover image`} />
         </div>
       ))}
     </div>
